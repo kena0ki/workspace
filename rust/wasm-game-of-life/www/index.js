93cf725b99aa1ee2980cecad49dba07a9a4e534c
kena0ki <ken3574.25@gmail.com>
2021-01-24 21:07:35 +0900

    <pre>

--- rust/wasm-game-of-life/www/index.js
+++ rust/wasm-game-of-life/www/index.js
@@ -1,4 +1,16 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet('jack');
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
+
+// wasm.greet('jack');
 
