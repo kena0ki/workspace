306391d0177304a21e3058a3953d082902ec61f4
Ken Aoki <ken3574.25@gmail.com>
2021-01-29 12:26:29 +0000

    upd

--- rust/dddl/src/lib.rs
+++ rust/dddl/src/lib.rs
@@ -1,5 +1,6 @@
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
+use sqlparser::ast::DataType;
 
 pub mod dddl {
     use super::*;
@@ -9,23 +10,113 @@ pub mod dddl {
     }
 }
 
-struct Type {
+pub struct Column {
+    pub content: String,
+    pub col_no: u32,
+}
+impl Column {
+    pub fn to_csv_string(&self, delimiter: &str) -> String {
+        let mut csv = String::from(self.content.as_str());
+        csv // TODO escape
+    }
 }
 
-struct Column {
-    content: String,
-    dataType: Type,
+pub struct Row {
+    pub columns: Vec<Column>,
+    pub line_no: u32,
+}
+pub enum QuotesPreference {
+    Always,
+    TypeDependent,
+}
+impl Row {
+    pub fn to_csv_string(&self, delimiter: &str, quotes: &QuotesPreference) -> String {
+        let mut csv = String::from("");
+        let mut iter = self.columns.iter();
+        if let Some(c) = iter.next() {
+            csv.push_str("\"");
+            let csv_str = c.to_csv_string(delimiter);
+            csv.push_str(csv_str.as_str());
+            csv.push_str("\"");
+            for c in iter {
+                csv.push_str(delimiter);
+                csv.push_str("\"");
+                let csv_str = c.to_csv_string(delimiter);
+                csv.push_str(csv_str.as_str());
+                csv.push_str("\"");
+            }
+        }
+        csv
+    }
 }
 
-struct Row {
-    columns: Vec<Column>,
-    no: u32,
+pub enum Constraints {
+    PrimaryKey,
+    Unique,
+    ForeignKey, // TODO necessary?
+    NotNull,
+}
+
+pub struct ColumnDefinition {
+    pub data_type: DataType,
+    pub constraints: Vec<Constraints>,
+    pub field_name: String,
+    pub col_no: u32,
 }
 
 pub struct Table {
-    rows: Vec<Row>,
+    pub rows: Vec<Row>,
+    pub col_defs: Vec<ColumnDefinition>,
 }
 
+impl Table {
+    pub fn to_csv_string(&self, delimiter: &str, quotes: &QuotesPreference, line_separator: &str, has_header: bool) -> String {
+        let mut csv = String::from("");
+        if has_header {
+            let mut iter = self.col_defs.iter();
+            let cur_opt = iter.next();
+            let mut next_opt = iter.next();
+            if let Some(mut cur) = cur_opt {
+                csv.push_str("\"");
+                while let Some(next) = next_opt {
+                    csv.push_str(cur.field_name.as_str());
+                    csv.push_str("\"");
+                    csv.push_str(delimiter);
+                    csv.push_str("\"");
+                    cur = next;
+                    next_opt = iter.next();
+                }
+                csv.push_str(cur.field_name.as_str());
+                csv.push_str("\"");
+            }
+        }
+        if has_header {
+            let mut iter = self.col_defs.iter();
+            if let Some(d) = iter.next() {
+                csv.push_str("\"");
+                let csv_str = &d.field_name;
+                csv.push_str(csv_str.as_str());
+                csv.push_str("\"");
+                for d in iter {
+                    csv.push_str(delimiter);
+                    csv.push_str("\"");
+                    let csv_str = &d.field_name;
+                    csv.push_str(csv_str.as_str());
+                    csv.push_str("\"");
+                }
+            }
+        }
+        for r in &self.rows {
+            let row_csv = r.to_csv_string(delimiter, quotes);
+            csv.push_str(row_csv.as_str());
+            csv.push_str(line_separator);
+        }
+        csv
+    }
+    pub fn to_insert_statements() {
+
+    }
+}
 
 pub mod generator {
     use super::*;
@@ -34,10 +125,16 @@ pub mod generator {
             rows: vec!(Row{
                 columns: vec!(Column{
                     content: "".into(),
-                    dataType: Type{}
+                    col_no: 0,
                 }),
-                no: 0,
-            })
+                line_no: 0,
+            }),
+            col_defs: vec!(ColumnDefinition {
+                data_type: DataType::Int,
+                constraints: [].into(),
+                field_name: "".into(),
+                col_no: 0,
+            }),
         }
     }
 }
