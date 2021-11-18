f03a5e21cae807f5ce53e7fa1cb2f5e8b17519e7
kena0ki <ken3574.25@gmail.com>
2021-11-18 16:42:48 +0900

    dfs

---
+++ rust/at-coder/src/bin/dfs_loop/main.rs
@@ -0,0 +1,55 @@
+use text_io::read;
+use std::collections::{HashMap,BTreeSet};
+
+//
+// https://en.wikipedia.org/wiki/Depth-first_search
+// DFS preordering non-recursive
+//
+// graph:
+//       1
+//      / \
+//     2   3
+//    / \ / \
+//   4   5   6
+//
+// input:
+// 6 6
+// 1 2
+// 1 3
+// 2 4
+// 2 5
+// 3 6
+// 3 5
+// expected:
+// 1 3 6 5 2 4
+fn main() {
+    let num_vert: usize = read!();
+    let num_edges: usize = read!();
+    let mut adj:HashMap<usize,BTreeSet<usize>> = HashMap::with_capacity(num_edges);
+    for _ in 0..num_edges {
+        let u: usize = read!();
+        let v: usize = read!();
+        adj.entry(u).or_default().insert(v);
+    }
+    let mut visited = 0;
+    let mut stack = Vec::new();
+    let mut result = Vec::with_capacity(num_vert);
+    for i in 1..num_vert+1 {
+        if (1 << i) & visited == 0 {
+            stack.push(i);
+            while let Some(vert) = stack.pop() {
+                if (1 << vert) & visited > 0 { // if the graph has cycles, vertices are possibly stacked in advance.
+                    continue;
+                }
+                for &v in adj.entry(vert).or_default().iter() {
+                    stack.push(v);
+                }
+                visited |= 1<<vert;
+                result.push(vert);
+            }
+        }
+    }
+    println!("{}", visited);
+    println!("{:?}", result);
+}
+
