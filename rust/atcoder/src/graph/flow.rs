ab620e44e9245c81976d0424f27a0f691e3c43d9
kena0ki <ken3574.25@gmail.com>
2021-11-29 22:02:23 +0900

    add flow to FlowEdge

--- rust/atcoder/src/graph/flow.rs
+++ rust/atcoder/src/graph/flow.rs
@@ -1,5 +1,5 @@
 //! Maximum flows, matchings, and minimum cuts.
-use std::collections::btree_set::IntoIter;
+use std::{collections::btree_set::IntoIter, iter::StepBy};
 
 use super::{Graph, InDegree};
 
@@ -9,6 +9,7 @@ pub struct FlowEdge {
     v: usize,
     cap: i64,
     cost: i64,
+    flow: i64,
 }
 
 impl Graph<FlowEdge> {
@@ -16,10 +17,10 @@ impl Graph<FlowEdge> {
         let idx = self.num_e();
         // add an edge
         self.adj.entry(u).or_default().insert(InDegree{ idx, v });
-        self.edges.push(FlowEdge { u, v, cap, cost });
+        self.edges.push(FlowEdge { u, v, cap, cost, flow:0 });
         // add a residual edge
         self.adj.entry(v).or_default().insert(InDegree{ idx: idx+1, v:u });
-        self.edges.push(FlowEdge { v:u, u:v, cap:rcap, cost: -cost });
+        self.edges.push(FlowEdge { v:u, u:v, cap:rcap, cost: -cost, flow:0 });
     }
 }
 
@@ -27,6 +28,7 @@ impl Graph<FlowEdge> {
 pub struct FlowGraph {
     /// Owned graph, managed by this FlowGraph object.
     pub graph: Graph<FlowEdge>,
+    distance: Vec<i64>,
 }
 
 impl FlowGraph {
@@ -37,6 +39,7 @@ impl FlowGraph {
     pub fn new(vmax: usize, emax_hint: usize) -> Self {
         Self {
             graph: Graph::new(vmax, 2 * emax_hint),
+            distance: vec![],
         }
     }
 
@@ -44,6 +47,33 @@ impl FlowGraph {
     /// flow. If only forward flow is allowed, rcap should be zero.
     pub fn add_edge(&mut self, u: usize, v: usize, cap: i64, rcap: i64, cost: i64) {
         self.graph.add_flow_edge(u,v,cap,rcap,cost);
+    }
+
+    /// Iterator of the edges not including residual edges.
+    pub fn edge_iter(&self) -> StepBy<std::slice::Iter<FlowEdge>>{
+        return self.graph.edges.iter().step_by(2);
+    }
+
+    /// Get an nth edge. The specified index corresponds to the order of adding edges.
+    pub fn get_edge(&self, n: usize) -> &FlowEdge{
+        return &self.graph.edges[n*2];
+    }
+
+    /// Underlying edges in the graph including residual edges.
+    pub fn edges_including_residual_edges(&self) -> &[FlowEdge]{
+        return &*self.graph.edges;
+    }
+
+    /// clear flow value once they are calculated.
+    pub fn clear_flow(&mut self) {
+        for e in self.graph.edges.iter_mut() {
+            e.flow = 0;
+        }
+    }
+
+    fn augment_path(&mut self, e: usize, flow: i64) {
+        self.graph.edges[e].flow += flow;
+        self.graph.edges[e ^ 1].flow -= flow;
     }
 
     /// Dinic's algorithm to find the maximum flow from s to t where s != t.
@@ -54,50 +84,45 @@ impl FlowGraph {
     /// # Panics
     ///
     /// Panics if the maximum flow is 2^63 or larger.
-    pub fn dinic(&self, s: usize, t: usize) -> (i64, Vec<i64>, Vec<i64>) {
-        let mut flow = vec![0; self.graph.num_e()];
+    pub fn dinic(&mut self, s: usize, t: usize) -> i64 {
         let mut max_flow = 0;
-        let mut dist;
         loop {
-            dist = self.dinic_search(s, &flow);
-            if dist[t] == Self::INF {
+            self.dinic_search(s);
+            if self.distance[t] == Self::INF {
                 break;
             }
             // Keep track of adjacency lists to avoid revisiting blocked edges.
             let mut adj_iters = (0..self.graph.num_v())
                 .map(|u| self.graph.adj_list(u).into_iter().peekable())
                 .collect::<Vec<_>>();
-            max_flow += self.dinic_augment(s, t, Self::INF, &dist, &mut adj_iters, &mut flow);
+            max_flow += self.dinic_augment(s, t, Self::INF, &mut adj_iters);
         }
-        (max_flow, flow, dist)
+        max_flow
     }
 
     // Compute BFS distances to restrict attention to shortest path edges.
-    fn dinic_search(&self, s: usize, flow: &[i64]) -> Vec<i64> {
-        let mut dist = vec![Self::INF; self.graph.num_v()];
+    fn dinic_search(&mut self, s: usize) {
         let mut q = ::std::collections::VecDeque::new();
-        dist[s] = 0;
+        self.distance = vec![Self::INF; self.graph.num_v()];
+        self.distance[s] = 0;
         q.push_back(s);
         while let Some(u) = q.pop_front() {
             for InDegree{idx:e, v} in self.graph.adj_list(u) {
-                if dist[v] == Self::INF && flow[e] < self.graph.edges[e].cap {
-                    dist[v] = dist[u] + 1;
+                if self.distance[v] == Self::INF && self.graph.edges[e].flow < self.graph.edges[e].cap {
+                    self.distance[v] = self.distance[u] + 1;
                     q.push_back(v);
                 }
             }
         }
-        dist
     }
 
     // Pushes a blocking flow that increases the residual's s-t distance.
     fn dinic_augment(
-        &self,
+        &mut self,
         u: usize,
         t: usize,
         flow_input: i64,
-        dist: &[i64],
         adj: &mut [::std::iter::Peekable<IntoIter<InDegree>>],
-        flow: &mut [i64],
     ) -> i64 {
         if u == t {
             return flow_input;
@@ -105,15 +130,14 @@ impl FlowGraph {
         let mut flow_used = 0;
 
         while let Some(&InDegree{idx:e, v}) = adj[u].peek() {
-            let cap = self.graph.edges[e].cap;
-            let rem_cap = (cap - flow[e]).min(flow_input - flow_used);// min(remaining capacity, remaining flow)
-            if rem_cap > 0 && dist[v] == dist[u] + 1 {
+            let edge = &self.graph.edges[e];
+            let rem_cap = (edge.cap - edge.flow).min(flow_input - flow_used);// min(remaining capacity, remaining flow)
+            if rem_cap > 0 && self.distance[v] == self.distance[u] + 1 {
                 // calculates maximum flow in a subtree (max_flow).
                 // max_flow never exceeds the remaining flow since rem_cap is not greater than
                 // the remaining flow.
-                let max_flow = self.dinic_augment(v, t, rem_cap, dist, adj, flow);
-                flow[e] += max_flow;
-                flow[e ^ 1] -= max_flow;
+                let max_flow = self.dinic_augment(v, t, rem_cap, adj);
+                self.augment_path(e, max_flow);
                 flow_used += max_flow; // add the maximum flow in a subtree
                 if flow_used == flow_input { // until the summary reaches to the input flow.
                     break;
@@ -126,11 +150,11 @@ impl FlowGraph {
     }
 
     /// After running maximum flow, use this to recover the dual minimum cut.
-    pub fn min_cut(&self, dist: &[i64]) -> Vec<usize> {
+    pub fn min_cut(&self) -> Vec<usize> {
         (0..self.graph.num_e())
             .filter(|&e| { // filter blocked edges
-                let edge = self.graph.edges[e];
-                dist[edge.u] < Self::INF && dist[edge.v] == Self::INF
+                let edge = &self.graph.edges[e];
+                self.distance[edge.u] < Self::INF && self.distance[edge.v] == Self::INF
             })
             .collect()
     }
@@ -141,7 +165,7 @@ impl FlowGraph {
     /// # Panics
     ///
     /// Panics if the flow or cost overflow a 64-bit signed integer.
-    pub fn mcf(&self, s: usize, t: usize) -> (i64, i64, Vec<i64>) {
+    pub fn mcf(&mut self, s: usize, t: usize) -> (i64, i64) {
         let mut pot = vec![0; self.graph.num_v()];
 
         // Bellman-Ford deals with negative-cost edges at initialization.
@@ -154,39 +178,37 @@ impl FlowGraph {
             }
         }
 
-        let mut flow = vec![0; self.graph.num_e()];
         let (mut min_cost, mut max_flow) = (0, 0);
         loop {
-            let par = self.mcf_search(s, &flow, &mut pot); // find shortest path from t to u.
+            let par = self.mcf_search(s, &mut pot); // find shortest path from t to u.
             if par[t] == None {
                 break;
             }
-            let (dc, df) = self.mcf_augment(t, &par, &mut flow);
+            let (dc, df) = self.mcf_augment(t, &par);
             min_cost += dc;
             max_flow += df;
         }
-        (min_cost, max_flow, flow)
+        (min_cost, max_flow)
     }
 
     // Maintains Johnson's potentials to prevent negative-cost residual edges.
     // This allows running Dijkstra instead of the slower Bellman-Ford.
-    fn mcf_search(&self, s: usize, flow: &[i64], pot: &mut [i64]) -> Vec<Option<usize>> {
+    fn mcf_search(&mut self, s: usize, pot: &mut [i64]) -> Vec<Option<usize>> {
         let mut vis = vec![false; self.graph.num_v()];
-        let mut dist = vec![Self::INF; self.graph.num_v()];
+        self.distance = vec![Self::INF; self.graph.num_v()];
         let mut par = vec![None; self.graph.num_v()];
 
-        dist[s] = 0;
+        self.distance[s] = 0;
         while let Some(u) = (0..self.graph.num_v())
-            .filter(|&u| !vis[u] && dist[u] < Self::INF)
-            .min_by_key(|&u| dist[u] - pot[u])
+            .filter(|&u| !vis[u] && self.distance[u] < Self::INF)
+            .min_by_key(|&u| self.distance[u] - pot[u])
         {
             vis[u] = true;
-            pot[u] = dist[u];
+            pot[u] = self.distance[u];
             for InDegree{idx:e, v} in self.graph.adj_list(u) {
-                println!("e: {:?}, v: {:?}", e, v);
                 let edge = &self.graph.edges[e];
-                if dist[v] > dist[u] + edge.cost && flow[e] < edge.cap {
-                    dist[v] = dist[u] + edge.cost;
+                if self.distance[v] > self.distance[u] + edge.cost && edge.flow < edge.cap {
+                    self.distance[v] = self.distance[u] + edge.cost;
                     par[v] = Some(e);
                 }
             }
@@ -195,28 +217,24 @@ impl FlowGraph {
     }
 
     // Pushes flow along an augmenting path of minimum cost.
-    fn mcf_augment(&self, t: usize, par: &[Option<usize>], flow: &mut [i64]) -> (i64, i64) {
+    fn mcf_augment(&mut self, t: usize, par: &[Option<usize>]) -> (i64, i64) {
         let (mut dc, mut df) = (0, Self::INF);
         let mut u = t;
-        println!("start");
         let mut i=0;
         while let Some(e) = par[u] {
             let edge = &self.graph.edges[e];
-            df = df.min(edge.cap - flow[e]);
+            df = df.min(edge.cap - edge.flow);
             u = edge.u;
-            println!("u: {}", u);
             i+=1;
             if i>10 {
                 break;
             }
 
         }
-        println!("end");
         u = t;
         while let Some(e) = par[u] {
+            self.augment_path(e, df);
             let edge = &self.graph.edges[e];
-            flow[e] += df;
-            flow[e ^ 1] -= df;
             dc += df * edge.cost;
             u = edge.u;
         }
@@ -229,7 +247,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_dinic2() {
+    fn test_dinic() {
         let mut graph = FlowGraph::new(5, 5);
         graph.add_edge(0, 1, 3, 0, 0);
         graph.add_edge(1, 2, 2, 0, 0);
@@ -237,10 +255,8 @@ mod test {
         graph.add_edge(2, 4, 2, 0, 0);
         graph.add_edge(3, 4, 2, 0, 0);
 
-        let (max, flow, dist) = graph.dinic(0, 4);
+        let max = graph.dinic(0, 4);
         println!("max: {:?}", max);
-        println!("flow: {:?}", flow);
-        println!("dist: {:?}", dist);
         assert_eq!(max, 3);
     }
 
@@ -250,12 +266,10 @@ mod test {
         graph.add_edge(0, 1, 4, 0, 0);
         graph.add_edge(1, 2, 3, 0, 0);
 
-        let (max, flow, dist) = graph.dinic(0, 2);
+        let max = graph.dinic(0, 2);
         println!("max: {:?}", max);
-        println!("flow: {:?}", flow);
-        println!("dist: {:?}", dist);
         assert_eq!(max, 3);
-        assert_eq!(&[2], &*graph.min_cut(&dist));
+        assert_eq!(&[2], &*graph.min_cut());
     }
 
     #[test]
@@ -266,7 +280,7 @@ mod test {
         graph.add_edge(2, 3, 7, 0, 8);
         graph.add_edge(1, 3, 7, 0, 10);
 
-        let (cost, flow, _) = graph.mcf(0, 3);
+        let (cost, flow) = graph.mcf(0, 3);
         assert_eq!(cost, 18);
         assert_eq!(flow, 10);
     }
@@ -301,24 +315,32 @@ mod test {
         graph.add_edge(left_start + 4, right_start + 3, 1, 0, 0);
         graph.add_edge(left_start + 5, right_start + 5, 1, 0, 0);
 
-        let (flow_amt, flow, _) = graph.dinic(source, sink);
+        let flow_amt = graph.dinic(source, sink);
         assert_eq!(flow_amt, 5);
 
-        //L->R edges in maximum matching
-        let left_right_edges = flow
-            .into_iter()
-            .enumerate()
-            .filter(|&(_e, f)| f > 0)
-            //map to u->v
-            .map(|(e, _f)| (graph.graph.edges[e].u, graph.graph.edges[e].v))
-            //leave out source and sink nodes
-            .filter(|&(u, v)| u != source && v != sink)
-            .collect::<Vec<_>>();
+        let mut matched_edges = graph.edge_iter()
+            .filter(|&e| e.flow>0 && e.u != source && e.v != sink);
+        assert_eq!(FlowEdge { u: 1, v: 8, cap: 1, cost: 0, flow: 1 },  *matched_edges.next().unwrap());
+        assert_eq!(FlowEdge { u: 3, v: 7, cap: 1, cost: 0, flow: 1 },  *matched_edges.next().unwrap());
+        assert_eq!(FlowEdge { u: 4, v: 9, cap: 1, cost: 0, flow: 1 },  *matched_edges.next().unwrap());
+        assert_eq!(FlowEdge { u: 5, v: 10, cap: 1, cost: 0, flow: 1 }, *matched_edges.next().unwrap());
+        assert_eq!(FlowEdge { u: 6, v: 12, cap: 1, cost: 0, flow: 1 }, *matched_edges.next().unwrap());
 
-        assert_eq!(
-            left_right_edges,
-            vec![(1, 8), (3, 7), (4, 9), (5, 10), (6, 12)]
-        );
+        // //L->R edges in maximum matching
+        // let left_right_edges = flow
+        //     .into_iter()
+        //     .enumerate()
+        //     .filter(|&(_e, f)| f > 0)
+        //     //map to u->v
+        //     .map(|(e, _f)| (graph.graph.edges[e].u, graph.graph.edges[e].v))
+        //     //leave out source and sink nodes
+        //     .filter(|&(u, v)| u != source && v != sink)
+        //     .collect::<Vec<_>>();
+
+        // assert_eq!(
+        //     left_right_edges,
+        //     vec![(1, 8), (3, 7), (4, 9), (5, 10), (6, 12)]
+        // );
     }
 }
 
