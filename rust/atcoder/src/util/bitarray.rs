f327228439d043b32e7856609296863e18c8c73f
kena0ki <ken3574.25@gmail.com>
2021-12-05 02:22:26 +0900

    bitarray

--- rust/atcoder/src/util/bitarray.rs
+++ rust/atcoder/src/util/bitarray.rs
@@ -1,4 +1,4 @@
-use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Shl, ShlAssign};
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Shl, ShlAssign, ShrAssign, Shr};
 
 
 #[derive(Debug,Clone)]
@@ -132,17 +132,17 @@ impl ShlAssign<usize> for BitArray {
             let offset = rhs % Self::BITS_PER_UNIT;
             let sub_offset = Self::BITS_PER_UNIT - offset;
 
-        if offset == 0 {
-            for i in (shift..self.num_arr).rev() {
-                self.bits[i] = self.bits[i - shift];
+            if offset == 0 {
+                for i in (shift..self.num_arr).rev() {
+                    self.bits[i] = self.bits[i - shift];
+                }
+            } else {
+                for i in (shift+1..self.num_arr).rev() {
+                    self.bits[i] = (self.bits[i - shift] << offset)
+                         | (self.bits[i - shift - 1] >> sub_offset);
+                }
+                self.bits[shift] = self.bits[0] << offset;
             }
-        } else {
-            for i in (shift+1..self.num_arr).rev() {
-                self.bits[i] = (self.bits[i - shift] << offset)
-                     | (self.bits[i - shift - 1] >> sub_offset);
-            }
-            self.bits[shift] = self.bits[0] << offset;
-        }
             self.bits[0..shift].fill(0);
             let unused_range = Self::BITS_PER_UNIT - self.num_bits%Self::BITS_PER_UNIT;
             self.bits[self.num_arr-1] &= !0 >> unused_range;
@@ -150,6 +150,35 @@ impl ShlAssign<usize> for BitArray {
     }
 }
 
+impl Shr<usize> for BitArray {
+    type Output = Self;
+    fn shr(mut self, rhs: usize) -> Self::Output {
+        self.shr_assign(rhs);
+        return self;
+    }
+}
+impl ShrAssign<usize> for BitArray {
+    fn shr_assign(&mut self, rhs: usize) {
+        if rhs != 0 {
+            let shift = rhs / Self::BITS_PER_UNIT;
+            let offset = rhs % Self::BITS_PER_UNIT;
+            let sub_offset = Self::BITS_PER_UNIT - offset;
+
+            if offset == 0 {
+                for i in shift..self.num_arr {
+                    self.bits[i-shift] = self.bits[i];
+                }
+            } else {
+                for i in shift..self.num_arr-1 {
+                    self.bits[i-shift] = (self.bits[i + 1] << sub_offset)
+                         | (self.bits[i] >> offset);
+                }
+                self.bits[self.num_arr-shift-1] = self.bits[self.num_arr-1] >> offset;
+            }
+            self.bits[self.num_arr-(shift.max(1))..self.num_arr-1].fill(0);
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -203,6 +232,24 @@ mod test {
         barr.set_bits_by_num(!0 - (1<<2) - (1<<80), 10);
         barr <<= 50;
         let expected = "00000000000011111111111111111111111111111111111111111111111011111111111111111111111111111111111111111111111111111111111111111111111111111011000000000000000000000000000000000000000000000000000000000000";
+        assert_eq!(expected, barr.to_string());
+    }
+
+    #[test]
+    fn barr_shift_right() {
+        let mut barr = BitArray::new(200);
+        barr.set_bits_by_num(!0 - (1<<2) - (1<<80), 72);
+        barr = barr >> 100;
+        let expected = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001111111111111111111111111111111111111111111111101111111111111111111111111111111111111111111111111111";
+        assert_eq!(expected, barr.to_string());
+    }
+
+    #[test]
+    fn barr_shift_right_assign() {
+        let mut barr = BitArray::new(200);
+        barr.set_bits_by_num(!0 - (1<<2) - (1<<80), 72);
+        barr >>= 50;
+        let expected = "00000000000000000000000000000000000000000000000000111111111111111111111111111111111111111111111110111111111111111111111111111111111111111111111111111111111111111111111111111110110000000000000000000000";
         assert_eq!(expected, barr.to_string());
     }
 }
