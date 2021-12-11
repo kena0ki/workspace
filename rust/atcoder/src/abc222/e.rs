aa3f9b3a6229dc169144380b333f27e0a5a9ddee
kena0ki <ken3574.25@gmail.com>
2021-12-11 18:48:54 +0900

    abc222

---
+++ rust/atcoder/src/abc222/e.rs
@@ -0,0 +1,37 @@
+// template
+
+use std::io::{BufRead, BufWriter, Write};
+use rustrithm::scanner;
+
+fn main() {
+    let sin = std::io::stdin();
+    let scan = &mut scanner::Scanner::new(sin.lock());
+    let sout = std::io::stdout();
+    let out = &mut BufWriter::new(sout.lock());
+    solve(scan, out);
+}
+
+fn solve(scan: &mut scanner::Scanner<impl BufRead>, out: &mut impl Write) {
+    let a: String = scan.token();
+    writeln!(out, "{} baz", a).ok();
+}
+
+#[cfg(test)]
+mod abc222e {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let input: &[u8] = b"\
+foo bar
+";
+        let expected = "\
+foo baz
+";
+        let output = &mut Vec::new();
+        let scan = &mut scanner::Scanner::new(input);
+        solve(scan, output);
+
+        assert_eq!(expected, std::str::from_utf8(output).unwrap());
+    }
+}
