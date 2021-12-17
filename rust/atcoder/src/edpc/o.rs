27df3b3857a251da204ebab529bcb34d067316b1
kena0ki <ken3574.25@gmail.com>
2021-12-17 20:54:25 +0900

    edpc_o WIP

---
+++ rust/atcoder/src/edpc/o.rs
@@ -0,0 +1,137 @@
+// template
+
+use std::io::{BufRead, BufWriter, Write};
+use rustrithm::scanner;
+
+fn main() {
+    let sin = std::io::stdin();
+    let scan = &mut scanner::Scanner::new(sin.lock());
+    let sout = std::io::stdout();
+    let out = &mut BufWriter::new(sout.lock());
+    solve(scan, out);
+}
+
+const MOD:u64 = 10000007;
+fn solve(scan: &mut scanner::Scanner<impl BufRead>, out: &mut impl Write) {
+    let n = scan.token::<usize>();
+    let mut a = vec![vec![0usize; n];n];
+    for i in 0..n { for j in 0..n {
+        let aij = scan.token::<usize>();
+        a[i][j]=aij;
+    }}
+    let mut dp = vec![vec![0u64;1<<n];n];
+    dp[0][0]=1;
+    for i in 0..n {
+        for bit in 0..1<<n {
+            for j in 0..n {
+                if bit >> j & 1 == 1 && a[i-1][j] == 1 {
+                    dp[i][bit] = dp[i][bit] + dp[i-1][bit ^ 1<<j];
+                }
+            }
+        }
+    }
+    writeln!(out, "{}", dp[n-1][1<<n-1]).ok();
+}
+
+#[allow(unused)]
+#[macro_export]
+macro_rules! logln {
+    ($($arg:tt)*) => ({
+        #[cfg(debug_assertions)]
+        println!($($arg)*);
+    })
+}
+
+#[cfg(test)]
+mod abc999x {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let input: &[u8] = b"\
+3
+0 1 1
+1 0 1
+1 1 1
+";
+        let expected = "\
+3
+";
+        let output = &mut Vec::new();
+        let scan = &mut scanner::Scanner::new(input);
+        solve(scan, output);
+
+        assert_eq!(expected, std::str::from_utf8(output).unwrap());
+    }
+
+    #[test]
+    fn test2() {
+        let input: &[u8] = b"\
+4
+0 1 0 0
+0 0 0 1
+1 0 0 0
+0 0 1 0
+";
+        let expected = "\
+1
+";
+        let output = &mut Vec::new();
+        let scan = &mut scanner::Scanner::new(input);
+        solve(scan, output);
+
+        assert_eq!(expected, std::str::from_utf8(output).unwrap());
+    }
+
+    #[test]
+    fn test3() {
+        let input: &[u8] = b"\
+1
+0
+";
+        let expected = "\
+0
+";
+        let output = &mut Vec::new();
+        let scan = &mut scanner::Scanner::new(input);
+        solve(scan, output);
+
+        assert_eq!(expected, std::str::from_utf8(output).unwrap());
+    }
+
+    #[test]
+    fn test4() {
+        let input: &[u8] = b"\
+21
+0 0 0 0 0 0 0 1 1 0 1 1 1 1 0 0 0 1 0 0 1
+1 1 1 0 0 1 0 0 0 1 0 0 0 0 1 1 1 0 1 1 0
+0 0 1 1 1 1 0 1 1 0 0 1 0 0 1 1 0 0 0 1 1
+0 1 1 0 1 1 0 1 0 1 0 0 1 0 0 0 0 0 1 1 0
+1 1 0 0 1 0 1 0 0 1 1 1 1 0 0 0 0 0 0 0 0
+0 1 1 0 1 1 1 0 1 1 1 0 0 0 1 1 1 1 0 0 1
+0 1 0 0 0 1 0 1 0 0 0 1 1 1 0 0 1 1 0 1 0
+0 0 0 0 1 1 0 0 1 1 0 0 0 0 0 1 1 1 1 1 1
+0 0 1 0 0 1 0 0 1 0 1 1 0 0 1 0 1 0 1 1 1
+0 0 0 0 1 1 0 0 1 1 1 0 0 0 0 1 1 0 0 0 1
+0 1 1 0 1 1 0 0 1 1 0 0 0 1 1 1 1 0 1 1 0
+0 0 1 0 0 1 1 1 1 0 1 1 0 1 1 1 0 0 0 0 1
+0 1 1 0 0 1 1 1 1 0 0 0 1 0 1 1 0 1 0 1 1
+1 1 1 1 1 0 0 0 0 1 0 0 1 1 0 1 1 1 0 0 1
+0 0 0 1 1 0 1 1 1 1 0 0 0 0 0 0 1 1 1 1 1
+1 0 1 1 0 1 0 1 0 0 1 0 0 1 1 0 1 0 1 1 0
+0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 1 1 0 0 1
+0 0 0 1 0 0 1 1 0 1 0 1 0 1 1 0 0 1 1 0 1
+0 0 0 0 1 1 1 0 1 0 1 1 1 0 1 1 0 0 1 1 0
+1 1 0 1 1 0 0 1 1 0 1 1 0 1 1 1 1 1 0 1 0
+1 0 0 1 1 0 1 1 1 1 1 0 1 0 1 1 0 0 0 0 0
+";
+        let expected = "\
+102515160
+";
+        let output = &mut Vec::new();
+        let scan = &mut scanner::Scanner::new(input);
+        solve(scan, output);
+
+        assert_eq!(expected, std::str::from_utf8(output).unwrap());
+    }
+}
