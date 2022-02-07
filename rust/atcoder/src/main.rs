cd8543427a0dbe45484849e813dee798c58b5df3
kena0ki <ken3574.25@gmail.com>
2022-02-07 17:42:21 +0900

    scanner

--- rust/atcoder/src/main.rs
+++ rust/atcoder/src/main.rs
@@ -1,20 +1,19 @@
 // template
 
 use std::io::{BufRead, BufWriter, Write};
-use rustrithm::scanner;
 #[allow(unused)]
 use std::collections::*;
 
 fn main() {
     let sin = std::io::stdin();
-    let scan = &mut scanner::Scanner::new(sin.lock());
+    let scan = &mut Scanner::new(sin.lock());
     let sout = std::io::stdout();
     let out = &mut BufWriter::new(sout.lock());
     solve(scan, out);
 }
 
 // https://atcoder.jp/contests/abc222/tasks/abc222_a
-fn solve(scan: &mut scanner::Scanner<impl BufRead>, out: &mut impl Write) {
+fn solve(scan: &mut Scanner<impl BufRead>, out: &mut impl Write) {
     let a = scan.token::<usize>();
     writeln!(out, "{} 3", a).ok();
 }
@@ -26,6 +25,30 @@ macro_rules! logln {
         #[cfg(debug_assertions)]
         println!($($arg)*);
     })
+}
+
+pub struct Scanner<R> {
+    reader: R,
+    buffer: Vec<String>,
+}
+impl<R: ::std::io::BufRead> Scanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self { reader, buffer: vec![] }
+    }
+    pub fn token<T: ::std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            self.reader.read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+    pub fn token_bytes(&mut self) -> Vec<u8> {
+        let s = self.token::<String>();
+        return s.as_bytes().into();
+    }
 }
 
 #[cfg(test)]
@@ -41,7 +64,7 @@ mod abc999x {
 1 3
 ";
         let output = &mut Vec::new();
-        let scan = &mut scanner::Scanner::new(input);
+        let scan = &mut Scanner::new(input);
         solve(scan, output);
 
         assert_eq!(expected, std::str::from_utf8(output).unwrap());
