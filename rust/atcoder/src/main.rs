ec25c299a9a90dd0c9a1d1a644dc6cc555247e71
kena0ki <ken3574.25@gmail.com>
2022-02-08 13:52:35 +0900

    test macro

--- rust/atcoder/src/main.rs
+++ rust/atcoder/src/main.rs
@@ -55,18 +55,22 @@ impl<R: ::std::io::BufRead> Scanner<R> {
 mod abc999x {
     use super::*;
 
-    #[test]
-    fn test1() {
-        let input: &[u8] = b"\
-1 2
-";
-        let expected = "\
-1 3
-";
-        let output = &mut Vec::new();
-        let scan = &mut Scanner::new(input);
-        solve(scan, output);
-
-        assert_eq!(expected, std::str::from_utf8(output).unwrap());
+    macro_rules! test_macro {
+        ($name:ident, $input:expr, $expected:expr) => {
+            #[test]
+            fn $name() {
+                let output = &mut Vec::new();
+                let scan = &mut Scanner::new($input as &[u8]);
+                solve(scan, output);
+                assert_eq!($expected, std::str::from_utf8(output).unwrap());
+            }
+        };
     }
+
+    test_macro!(test1, b"\
+1 2
+" , "\
+1 3
+");
+
 }
