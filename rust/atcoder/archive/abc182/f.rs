9d2d079e33ac81c5c8bc9bbcd1c7e91df433b1db
kena0ki <ken3574.25@gmail.com>
2022-03-16 20:26:03 +0900

    abc182

--- rust/atcoder/archive/abc182/f.rs
+++ rust/atcoder/archive/abc182/f.rs
@@ -1,111 +1,15 @@
 // template
 
-use std::{io::{BufRead, BufWriter, Write}, collections::{HashSet, HashMap}};
-use rustrithm::scanner;
+use std::io::{BufRead, BufWriter, Write};
+#[allow(unused)]
+use std::collections::*;
 
 fn main() {
     let sin = std::io::stdin();
-    let scan = &mut scanner::Scanner::new(sin.lock());
+    let scan = &mut Scanner::new(sin.lock());
     let sout = std::io::stdout();
     let out = &mut BufWriter::new(sout.lock());
     solve(scan, out);
-}
-
-// https://atcoder.jp/contests/abc182/tasks/abc182_f
-fn solve(scan: &mut scanner::Scanner<impl BufRead>, out: &mut impl Write) {
-    let n = scan.token::<usize>();
-    let x = scan.token::<usize>();
-    let mut a = vec![0;n];
-    for i in 0..n {
-        a[i] = scan.token::<usize>();
-    }
-
-    let mut memo = HashMap::<(usize,usize),usize>::new();
-    let ans = f(x, 1, &a, n, &mut memo);
-    logln!("{:?}", memo);
-    writeln!(out, "{}", ans).ok();
-
-
-    fn f(x:usize, i:usize, a:&Vec<usize>, n:usize,memo: &mut HashMap<(usize,usize),usize>) -> usize {
-        //logln!("x: {} {}",x, i);
-        if memo.contains_key(&(i,x)) {
-            return memo[&(i,x)];
-        }
-        if i >= n {
-            memo.insert((i,x),1);
-            return 1;
-        }
-        if x == 0 {
-            logln!("{}",i);
-            memo.insert((i,x),1);
-            return 1;
-        }
-        let mut res = 0;
-        if x % a[i] > 0 {
-            let nx = x/a[i] * a[i];
-            res += f(nx, i+1, a, n, memo);
-            let nx = (x+a[i]-1)/a[i] * a[i];
-            res += f(nx, i+1, a, n, memo);
-        } else {
-            res += f(x, i+1, a, n, memo);
-        }
-        memo.insert((i,x), res);
-        return res;
-    }
-}
-
-// https://atcoder.jp/contests/abc182/tasks/abc182_f
-fn _solve_not_opti(scan: &mut scanner::Scanner<impl BufRead>, out: &mut impl Write) {
-    let n = scan.token::<usize>();
-    let x = scan.token::<usize>();
-    let mut a = vec![0;n];
-    for i in 0..n {
-        a[i] = scan.token::<usize>();
-    }
-
-    let mut set = HashSet::<usize>::new();
-    let mut memo = HashSet::<(usize,usize)>::new();
-    f(0, x, 0, &a, n, &mut set, x, &mut memo);
-    //logln!("{:?}", set);
-    writeln!(out, "{}", set.len()).ok();
-
-
-    fn f(y:usize, x:usize, i:usize, a:&Vec<usize>, n:usize, set: &mut HashSet<usize>, xx:usize, memo: &mut HashSet<(usize,usize)>) {
-        if memo.contains(&(i,y)) {
-            return;
-        }
-        memo.insert((i,y));
-        if i >= n {
-            //logln!("x,y: {} {} {}",x, y, i);
-            let ny = y + (x+a[i-1]-1)/a[i-1] * a[i-1];
-            set.insert(ny);
-            return;
-        }
-        if y >= xx {
-            //logln!("{} {} {}", y, x, i);
-            set.insert(y);
-            return;
-        }
-        if x <= a[i] {
-            //logln!("{} {} {}", y, x, i);
-            for j in i..n {
-                set.insert(y+a[j]);
-            }
-            return;
-        }
-        if x % a[i] > 0 {
-            let nx = x/a[i] * a[i];
-            let ny = y+x%a[i];
-            f(ny, nx, i+1, a, n, set, xx, memo);
-            let nx = (x+a[i]-1)/a[i] * a[i];
-            f(y, nx, i+1, a, n, set, xx, memo);
-        } else {
-            f(y, x, i+1, a, n, set, xx, memo);
-        }
-        let ny = y+(x+a[i]-1)/a[i] * a[i];
-        f(ny, 0, i+1, a, n, set, xx, memo);
-        return;
-    }
 }
 
 #[allow(unused)]
@@ -117,136 +21,118 @@ macro_rules! logln {
     })
 }
 
-#[cfg(test)]
-mod abc182f {
-    use super::*;
-
-    //#[test]
-    fn _test0() {
-        let input: &[u8] = b"\
-50 1001
-1
-2
-4
-8
-16
-32
-64
-128
-256
-512
-1024
-2048
-4096
-8192
-16384
-32768
-65536
-131072
-262144
-524288
-1048576
-2097152
-4194304
-8388608
-16777216
-33554432
-67108864
-134217728
-268435456
-536870912
-1073741824
-2147483648
-4294967296
-8589934592
-17179869184
-34359738368
-68719476736
-137438953472
-274877906944
-549755813888
-1099511627776
-2199023255552
-4398046511104
-8796093022208
-17592186044416
-35184372088832
-70368744177664
-140737488355328
-281474976710656
-562949953421312
-";
-        let expected = "\
-3
-";
-        let output = &mut Vec::new();
-        let scan = &mut scanner::Scanner::new(input);
-        solve(scan, output);
-
-        assert_eq!(expected, std::str::from_utf8(output).unwrap());
+pub struct Scanner<R> {
+    reader: R,
+    buffer: Vec<String>,
+}
+impl<R: ::std::io::BufRead> Scanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self { reader, buffer: vec![] }
     }
-
-    #[test]
-    fn test1() {
-        let input: &[u8] = b"\
-3 9
-1 5 10
-";
-        let expected = "\
-3
-";
-        let output = &mut Vec::new();
-        let scan = &mut scanner::Scanner::new(input);
-        solve(scan, output);
-
-        assert_eq!(expected, std::str::from_utf8(output).unwrap());
+    pub fn token<T: ::std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            self.reader.read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
     }
-
-    #[test]
-    fn test2() {
-        let input: &[u8] = b"\
-5 198
-1 5 10 50 100
-";
-        let expected = "\
-5
-";
-        let output = &mut Vec::new();
-        let scan = &mut scanner::Scanner::new(input);
-        solve(scan, output);
-
-        assert_eq!(expected, std::str::from_utf8(output).unwrap());
-    }
-
-    #[test]
-    fn test3() {
-        let input: &[u8] = b"\
-4 44
-1 4 20 100
-";
-        let expected = "\
-4
-";
-        let output = &mut Vec::new();
-        let scan = &mut scanner::Scanner::new(input);
-        solve(scan, output);
-
-        assert_eq!(expected, std::str::from_utf8(output).unwrap());
-    }
-
-    #[test]
-    fn test4() {
-        let input: &[u8] = b"\
-9 11837029798
-1 942454037 2827362111 19791534777 257289952101 771869856303 3859349281515 30874794252120 216123559764840
-";
-        let expected = "\
-20
-";
-        let output = &mut Vec::new();
-        let scan = &mut scanner::Scanner::new(input);
-        solve(scan, output);
-
-        assert_eq!(expected, std::str::from_utf8(output).unwrap());
+    pub fn token_bytes(&mut self) -> Vec<u8> {
+        let s = self.token::<String>();
+        return s.as_bytes().into();
     }
 }
+
+#[cfg(test)]
+mod abc999x {
+    use super::*;
+
+    macro_rules! test_macro {
+        ($name:ident, $input:expr, $expected:expr) => {
+            #[test]
+            fn $name() {
+                let output = &mut Vec::new();
+                let scan = &mut Scanner::new($input as &[u8]);
+                solve(scan, output);
+                assert_eq!($expected, std::str::from_utf8(output).unwrap());
+            }
+        };
+    }
+
+    test_macro!(test0, b"\
+3 1
+1 10 100
+" , "\
+3
+");
+
+    test_macro!(test1, b"\
+3 9
+1 5 10
+" , "\
+3
+");
+
+    test_macro!(test2, b"\
+5 198
+1 5 10 50 100
+" , "\
+5
+");
+
+    test_macro!(test3, b"\
+4 44
+1 4 20 100
+" , "\
+4
+");
+
+    test_macro!(test4, b"\
+9 11837029798
+1 942454037 2827362111 19791534777 257289952101 771869856303 3859349281515 30874794252120 216123559764840
+" , "\
+21
+");
+
+}
+
+// https://atcoder.jp/contests/abc182/tasks/abc182_f
+fn solve(scan: &mut Scanner<impl BufRead>, out: &mut impl Write) {
+    let n = scan.token::<usize>();
+    let x = scan.token::<usize>();
+    let mut va = Vec::with_capacity(n);
+    for _ in 0..n {
+        let a = scan.token::<usize>();
+        va.push(a);
+    }
+    va.push((1<<60)/va[n-1]*va[n-1]);
+    logln!("{:?}",va);
+    let n = n+1;
+    let mut memo = vec![HashMap::with_capacity(n);n];
+    let ans = f(x,1,n,&va,0,&mut memo);
+    fn f(x:usize,i:usize,n:usize,va:&Vec<usize>,y:usize,memo: &mut Vec<HashMap<usize,Option<usize>>>) -> usize {
+        if x == 0 {
+            logln!("{},{},{}",x,i,y);
+            return 1;
+        }
+        if i>=n { return 0; }
+        if memo[i].contains_key(&x) {
+            logln!("{:?}",memo);
+            return memo[i][&x].unwrap();
+        }
+        let mut res = 0;
+        let a = va[i];
+        if x%a > 0 {
+            let nx = x/a*a;
+            res += f(nx,i+1,n,va,y+x%a,memo);
+        }
+        let nx = (x+a-1)/a*a;
+        res += f(nx,i+1,n,va,y,memo);
+        *memo[i].entry(x).or_default() = Some(res);
+        return res;
+    }
+    writeln!(out, "{}", ans).ok();
+}
+
