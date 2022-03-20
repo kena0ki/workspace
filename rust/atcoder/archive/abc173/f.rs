1bbe30d76ccc759210bf571ea48d33839060f062
kena0ki <ken3574.25@gmail.com>
2022-03-20 11:31:36 +0900

    abc173

--- rust/atcoder/archive/abc173/f.rs
+++ rust/atcoder/archive/abc173/f.rs
@@ -12,56 +12,6 @@ fn main() {
     solve(scan, out);
 }
 
-// https://atcoder.jp/contests/abc173/tasks/abc173_f
-fn solve(scan: &mut Scanner<impl BufRead>, out: &mut impl Write) {
-    let n = scan.token::<usize>();
-    let mut e = vec![Vec::new();n];
-    let mut e2 = vec![Vec::new();n];
-    for _ in 0..n-1 {
-        let mut u = scan.token::<usize>() -1;
-        let mut v = scan.token::<usize>() -1;
-        if u < v {
-            std::mem::swap(&mut u,&mut v);
-        }
-        e[u].push(v);
-        e2[v].push(u);
-        logln!("{}->{}", u,v);
-    }
-    let mut cnts = vec![0;n];
-    logln!("{:?}",e);
-    cnts[0] = 1;
-    for i in 0..n-1 {
-        cnts[i+1] = cnts[i] + 1 - e[i+1].len();
-    }
-    let mut ans = 0;
-    let mut sum = cnts.iter().sum::<usize>();
-    for i in 0..n {
-        logln!("{}", sum);
-        ans += sum;
-        for &v in &e2[i] {
-            sum += n-v;
-        }
-        sum = sum - (n-i);
-    }
-    // let mut ans = 0;
-    // for i in 0..n {
-    //     let mut base = 0;
-    //     for j in i..n {
-    //         for &v in &e[j] {
-    //             if i>0 && v == i-1 {
-    //                 base += 1;
-    //                 break;
-    //             }
-    //         }
-    //         cnts[j] += base;
-    //         if i > 0 {cnts[j] -= 1};
-    //         ans += cnts[j];
-    //     }
-    //     logln!("{:?}", cnts);
-    // }
-    writeln!(out, "{}", ans).ok();
-}
-
 #[allow(unused)]
 #[macro_export]
 macro_rules! logln {
@@ -96,7 +46,7 @@ impl<R: ::std::io::BufRead> Scanner<R> {
 }
 
 #[cfg(test)]
-mod abc173f {
+mod abc999x {
     use super::*;
 
     macro_rules! test_macro {
@@ -118,12 +68,14 @@ mod abc173f {
 " , "\
 7
 ");
+
     test_macro!(test2, b"\
 2
 1 2
 " , "\
 3
 ");
+
     test_macro!(test3, b"\
 10
 5 3
@@ -140,3 +92,30 @@ mod abc173f {
 ");
 
 }
+
+// https://atcoder.jp/contests/abc173/tasks/abc173_f
+fn solve(scan: &mut Scanner<impl BufRead>, out: &mut impl Write) {
+    let n = scan.token::<usize>();
+    let mut adj = vec![Vec::with_capacity(n);n];
+    for _ in 0..n-1 {
+        let mut u = scan.token::<usize>()-1;
+        let mut v = scan.token::<usize>()-1;
+        if u < v {
+            std::mem::swap(&mut u, &mut v);
+        }
+        adj[u].push(v);
+    }
+    let mut ans = 0;
+    for i in 1..n+1 {
+        ans += i*(i+1)/2;
+    }
+    for i in 0..n {
+        let rest = n-i;
+        for &v in &adj[i] {
+            let sum = rest*(v+1);
+            ans -= sum;
+        }
+    }
+    writeln!(out, "{}", ans).ok();
+}
+
