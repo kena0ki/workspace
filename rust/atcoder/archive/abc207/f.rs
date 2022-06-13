ff0a104b560a1983b49708c1e528c542a2e9a255
kena0ki <ken3574.25@gmail.com>
2022-06-13 16:23:36 +0900

    abc207

--- rust/atcoder/archive/abc207/f.rs
+++ rust/atcoder/archive/abc207/f.rs
@@ -1,6 +1,25 @@
 // template
 
 use std::io::{BufRead, BufWriter, Write};
+#[allow(unused)]
+use std::collections::*;
+
+fn main() {
+    let sin = std::io::stdin();
+    let scan = &mut Scanner::new(sin.lock());
+    let sout = std::io::stdout();
+    let out = &mut BufWriter::new(sout.lock());
+    solve(scan, out);
+}
+
+#[allow(unused)]
+#[macro_export]
+macro_rules! logln {
+    ($($arg:tt)*) => ({
+        #[cfg(debug_assertions)]
+        println!($($arg)*);
+    })
+}
 
 pub struct Scanner<R> {
     reader: R,
@@ -26,133 +45,49 @@ impl<R: ::std::io::BufRead> Scanner<R> {
     }
 }
 
-fn main() {
-    let sin = std::io::stdin();
-    let scan = &mut Scanner::new(sin.lock());
-    let sout = std::io::stdout();
-    let out = &mut BufWriter::new(sout.lock());
-    solve(scan, out);
-}
-
-// https://atcoder.jp/contests/abc207/tasks/abc207_f
-// WIP
-fn solve(scan: &mut Scanner<impl BufRead>, out: &mut impl Write) {
-    let n = scan.token::<usize>();
-    let mut adj = vec![Vec::<usize>::with_capacity(n);n];
-    for _ in 0..n-1 {
-        let u = scan.token::<usize>()-1;
-        let v = scan.token::<usize>()-1;
-        adj[u].push(v);
-        adj[v].push(u);
-    }
-
-    const MOD: usize = 1000000007;
-    let res = f(&adj, 0, usize::max_value());
-    fn f(adj: &Vec<Vec<usize>>, u: usize, p:usize) -> Vec<Vec<usize>> {
-        if adj[u].len() <= 1 && p != usize::max_value() {
-            return vec![vec![0,1],vec![1,0]];
-        }
-        let mut res = vec![vec![0,1]];
-        for &v in &adj[u] {
-            if v == p { continue; }
-            let res2 = f(adj,v,u);
-            let len = res.len()+res2.len()-1;
-            let mut nxt = vec![vec![0;2];len];
-            for i1 in 0..res.len() { for i2 in 0..res2.len() {
-                for j1 in 0..3 { for j2 in 0..3 {
-                    if j1 == 1 || j2 == 1 {
-                        nxt[i1+i2][1] += res[i1][j1] * res2[i2][j2];
-                        nxt[i1+i2][1] %= MOD;
-                    } else {
-                        nxt[i1+i2][0] += res[i1][j1] * res2[i2][j2];
-                        nxt[i1+i2][0] %= MOD;
-                    }
-                }}
-                logln!("{:?}", nxt);
-            }}
-            let mut nxt2 = vec![vec![0;2];len];
-            for i in 0..len { for j in 0..2 { for nj in 0..2 {
-                let ni = if j == 0 && nj == 0 {
-                    i
-                } else if j == 1 || nj == 1 {
-                    i+1
-                } else { // nj == 1 && j == 0
-                    i+2
-                };
-                if nxt[i][j] > 0 {
-                    nxt2[ni][nj] = nxt[i][j];
-                }
-            }}}
-            res = nxt2;
-        }
-        return res;
-    }
-    for i in 0..n+1 {
-        let ans = if res.len() > i { res[i][0] + res[i][1] } else { 0 };
-        writeln!(out, "{}", ans).ok();
-    }
-}
-
-#[allow(unused)]
-#[macro_export]
-macro_rules! logln {
-    ($($arg:tt)*) => ({
-        #[cfg(debug_assertions)]
-        println!($($arg)*);
-    })
-}
-
 #[cfg(test)]
-mod abc208e {
+mod abc999x {
     use super::*;
 
-    #[test]
-    fn test1() {
-        let input: &[u8] = b"\
+    macro_rules! test_macro {
+        ($name:ident, $input:expr, $expected:expr) => {
+            #[test]
+            fn $name() {
+                let output = &mut Vec::new();
+                let scan = &mut Scanner::new($input as &[u8]);
+                solve(scan, output);
+                assert_eq!($expected, std::str::from_utf8(output).unwrap());
+            }
+        };
+    }
+
+    test_macro!(test1, b"\
 3
 1 3
 1 2
-";
-        let expected = "\
+" , "\
 1
 0
 2
 5
-";
-        let output = &mut Vec::new();
-        let scan = &mut Scanner::new(input);
-        solve(scan, output);
+");
 
-        assert_eq!(expected, std::str::from_utf8(output).unwrap());
-    }
-
-    #[test]
-    fn test2() {
-        let input: &[u8] = b"\
+    test_macro!(test2, b"\
 5
 1 3
 4 5
 1 5
 2 3
-";
-        let expected = "\
+" , "\
 1
 0
 2
 5
 7
 17
-";
-        let output = &mut Vec::new();
-        let scan = &mut Scanner::new(input);
-        solve(scan, output);
+");
 
-        assert_eq!(expected, std::str::from_utf8(output).unwrap());
-    }
-
-    #[test]
-    fn test3() {
-        let input: &[u8] = b"\
+    test_macro!(test3, b"\
 10
 6 10
 1 8
@@ -163,8 +98,7 @@ mod abc208e {
 7 10
 4 9
 2 8
-";
-        let expected = "\
+" , "\
 1
 0
 3
@@ -176,11 +110,67 @@ mod abc208e {
 196
 266
 325
-";
-        let output = &mut Vec::new();
-        let scan = &mut Scanner::new(input);
-        solve(scan, output);
+");
 
-        assert_eq!(expected, std::str::from_utf8(output).unwrap());
+}
+
+const MOD:usize = 1000000007;
+
+// https://atcoder.jp/contests/abc222/tasks/abc222_a
+fn solve(scan: &mut Scanner<impl BufRead>, out: &mut impl Write) {
+    let n = scan.token::<usize>();
+    let mut va = vec![Vec::new();n];
+    for _ in 0..n-1 {
+        let u = scan.token::<usize>()-1;
+        let v = scan.token::<usize>()-1;
+        va[u].push(v);
+        va[v].push(u);
+    }
+    let mp = dfs(&va, 0, usize::max_value());
+    for i in 0..n+1 {
+        let mut ans = 0;
+        let v = mp.get(&i);
+        if v.is_none() {
+            writeln!(out, "{}", ans).ok();
+            continue;
+        }
+        let v = v.unwrap();
+        for i in 0..3 {
+            ans += v[i];
+            ans %= MOD;
+        }
+        writeln!(out, "{}", ans).ok();
+    }
+
+    fn dfs(va: &Vec<Vec<usize>>, u:usize, p:usize) -> HashMap<usize,Vec<usize>> {
+        let mut res = HashMap::new();
+        res.insert(0,vec![1,0,0]);
+        res.insert(1,vec![0,0,1]);
+        for &v in &va[u] {
+            if v == p { continue; }
+            let chl = dfs(va,v,u);
+            let pre = res.clone();
+            let mut nxt = HashMap::new();
+            for (&pk,pv) in &pre { for (&ck,cv) in &chl {
+                for pi in 0..3 { for ci in 0..3 {
+                    if pv[pi]*cv[ci] == 0 { continue; }
+                    let mut ni = pi;
+                    if ni == 0 && ci == 2 {
+                        ni = 1;
+                    }
+                    let mut add = 0;
+                    if (pi == 2 && ci == 0 ) || (ci == 2 && pi == 0 ) {
+                        add=1;
+                    }
+                    let nv = nxt.entry(pk+ck+add).or_insert(vec![0,0,0]);
+                    nv[ni] += pv[pi]*cv[ci];
+                    nv[ni] %= MOD;
+                }}
+            }}
+            res = nxt;
+            logln!("{},{:?}",u,res);
+        }
+        return res;
     }
 }
+
