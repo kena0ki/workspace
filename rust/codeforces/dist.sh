d798ad1b237097df16b260ff8ea33e4659e5f7a0
kena0ki <ken3574.25@gmail.com>
2022-06-23 04:16:29 +0900

    codeforces

---
+++ rust/codeforces/dist.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+cp ./src/main.rs ./src/contest/a.rs
+cp ./src/main.rs ./src/contest/b.rs
+cp ./src/main.rs ./src/contest/c.rs
+cp ./src/main.rs ./src/contest/d.rs
+cp ./src/main.rs ./src/contest/e.rs
+cp ./src/main.rs ./src/contest/f.rs
+cp ./src/main.rs ./src/contest/g.rs
+cp ./src/main.rs ./src/contest/h.rs
+
